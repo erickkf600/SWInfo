0476e3311da17c9140604d314579e770e3143d8a
erickkf600 <erickkf600@gmail.com>
2019-04-10 20:25:10 -0300

    interface

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      planets: [],
+      isLoaded: false,
+    }
+  }
   render() {
     return (
       <div className="App">
