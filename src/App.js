73390beab27424b829f5003184dea3875efad225
erickkf600 <erickkf600@gmail.com>
2019-04-10 20:06:57 -0300

    interface

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react';
-import Axios from 'axios';
-import List from './List';
-
-/* Inicialmente Irrelevantes 
-*import logo from './logo.svg';
-* import './App.css'; */
+import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      planets: [],
-    }
-    this.getPlanets = this.getPlanets.bind(this);
-  }
-  //INSERIR O LINK DA API
-  getPlanets(){
-    return Axios.get("https://swapi.co/api/planets").then((response) => {
-      console.log(response.data.results);
-      this.setState({ planets: response.data.results})
-    })
-  }
-  //EXIBE OS ITENS EM UM ARRAY
-  componentDidMount(){
-    this.getPlanets()
-  }
-  //CHAMA OUTRO COMPONENTE QUE SEPARA OS ARRAYS
   render() {
-    const {planets} = this.state;
     return (
       <div className="App">
-      <List planets = {planets} /> 
+        
       </div>
     );
   }
